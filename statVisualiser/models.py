1e7e9b500ace37d3c8d8e970fe8c7d9ea1b288f1
ltptnt <ltptnt04@gmail.com>
2022-07-15 18:52:02 +1000

    add database

--- statVisualiser/models.py
+++ statVisualiser/models.py
@@ -2,11 +2,17 @@ from django.core.validators import *
 from django.db import models
 
 
-class Distributions(models.Model):
-    name = models.CharField(max_length=20)
-    supported_region = models.CharField(max_length=20)
-    param = models.CharField(max_length=100)
+class Distribution(models.Model):
 
-    def __str__(self):
+    def get_empty(self) -> dict:
+        return dict()
+
+    name = models.CharField(max_length=50)
+    continuous = models.BooleanField(blank=False)
+    supportedRegionMin = models.BigIntegerField(blank=True, default=9223372036854775807)
+    supportedRegionMax = models.BigIntegerField(blank=True, default=-9223372036854775808)
+    requiredVariableCount = models.IntegerField(blank=False, default=0)
+    requiredVariableNames = models.JSONField(blank=True, default=get_empty)
+
+    def __str__(self) -> str:
         return self.name
-
