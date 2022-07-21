140298c2a3bac948d17e8ec82c977244ad5da8f4
ltptnt <ltptnt04@gmail.com>
2022-07-21 16:01:13 +1000

    removed the supported regions as no longer required

--- statVisualiser/models.py
+++ statVisualiser/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Distribution(models.Model):
     name = models.CharField(max_length=50)
     continuous = models.BooleanField(blank=False)
-    supported_region_min = models.BigIntegerField(blank=True, default=-9223372036854775808)
-    supported_region_max = models.BigIntegerField(blank=True, default=9223372036854775807)
     required_variable_count = models.IntegerField(blank=False, default=0)
     required_variable_names = models.JSONField(blank=True)
 
