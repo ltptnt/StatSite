cc0a09afed73b6a1d9fbe2bb40e720f29bcab932
ltptnt <ltptnt04@gmail.com>
2022-07-21 16:00:40 +1000

    changed form from DecimalField to FloatField and changed the output from data to cleaned_data, allowing for the form to validated before the data is collected

--- statVisualiser/forms.py
+++ statVisualiser/forms.py
@@ -1,41 +1,28 @@
 from django import forms
 from .models import Distribution
-from django.core.exceptions import ValidationError
 import json
+from django.core.exceptions import ValidationError
 
 
-class DistributionPicker(forms.Form):
-    Type = forms.ModelChoiceField(queryset=Distribution.objects.all(), required=True)
-    Rate = forms.DecimalField(required=False)
-    Min = forms.DecimalField(required=False)
-    Max = forms.DecimalField(required=False)
-    Mean = forms.DecimalField(required=False)
-    Sd = forms.DecimalField(required=False)
-    Probability = forms.DecimalField(min_value=0, max_value=1, required=False)
-    Trials = forms.IntegerField(min_value=0, required=False)
+class DistributionSelect(forms.Form):
+    Type = forms.ModelChoiceField(label='Distribution', queryset=Distribution.objects.all(), required=True)
+    Rate = forms.FloatField(label='Rate', required=False)
+    Rate = forms.FloatField(label='Rate', required=False)
+    Max = forms.FloatField(label='Maximum Value', required=False)
+    Mean = forms.FloatField(label='Mean', required=False)
+    Sd = forms.FloatField(label='Standard Deviation', required=False)
+    Probability = forms.FloatField(label='Probability', min_value=0, max_value=1, required=False)
+    Trials = forms.IntegerField(label='Trials', min_value=0, required=False)
 
-    def get_data(self) -> int:
+    def get_data(self) -> dict[str, float] | dict[str, int]:
         data = dict()
-
         dist = self.cleaned_data.get("Type")
         stored_info = Distribution.objects.get(name=dist)
-        print(stored_info.required_variable_names)
         required_variables = json.loads(json.dumps(stored_info.required_variable_names))
 
         for variable in required_variables:
-            if variable == "Min":
-                if stored_info.supported_region_min < self.cleaned_data.get("Min") < stored_info.supported_region_max:
-                    data["Min"] = self.cleaned_data.get("Min")
-                else:
-                    raise ValidationError("Your Minimium value is not within the supported range!")
-
-            if variable == "Max":
-                if stored_info.supported_region_max > self.cleaned_data.get("Max") > stored_info.supported_region_min:
-                    data["Max"] = self.cleaned_data.get("Max")
-                else:
-                    raise ValidationError("Your Maximium value is not within the supported range!")
-
-            else:
-                data[variable] = self.data.get(variable)
+            if self.cleaned_data.get(variable) == None:
+                raise ValidationError('Please input a value for ' + variable)
+            data[variable] = self.cleaned_data.get(variable)
 
         return data
