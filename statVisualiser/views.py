623634e08421a7b7fb02dacfc072445e63651724
ltptnt <ltptnt04@gmail.com>
2022-07-22 23:08:02 +1000

    Cleaned up

--- statVisualiser/views.py
+++ statVisualiser/views.py
@@ -1,11 +1,7 @@
-from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.template import loader
 from .forms import *
 from .util.distributions import *
-from plotly.io import to_html
-
-from .util.distributions import Exponential, Uniform, Normal, Poisson, Bernoulli, Binomial
 
 
 def index(request):
@@ -15,8 +11,8 @@ def index(request):
 
 
 def distributions(request):
-    dist_one_select = DistributionSelect(prefix='picker1')
-    dist_two_select = DistributionSelect(prefix='picker2')
+    dist_one_select = DistributionSelect(auto_id=True, prefix='picker1')
+    dist_two_select = DistributionSelect(auto_id=True, prefix='picker2')
     dist_table = Distribution.objects.all().values()
     template = loader.get_template('statVisualiser/distributions.html')
 
@@ -63,6 +59,7 @@ def dist_selector(picker: DistributionSelect) -> Exponential | Uniform | Normal 
             return Binomial(data.get('Trials', 0), data.get('Probability', 0))
         case _:
             return None
+
 # Need Large numbers then wheel spin here. Ordered as seen on website navigation bar to avoid confusion
 
 
@@ -70,69 +67,3 @@ def about(request):
     template = loader.get_template('statVisualiser/about.html')
     context = {}
     return HttpResponse(template.render(context, request))
-
-
-
-
-
-
-
-"""
-def index(request):
-    graph = ""
-    if request.method == "POST":
-        form = Picker(request.POST)
-        if form.is_valid():
-            data_id = int(str(form.cleaned_data.get("Data")))
-            equation_name = form.cleaned_data.get("Equation")
-            data = Data.objects.get(id=data_id).data
-            model = StatModel.objects.get(name=equation_name).id
-            graph = makeGraph(model, data)
-    models = StatModel.objects.all().values()
-    data = Data.objects.all().values()
-    data_picker = Picker()
-    template = loader.get_template('statVisualiser/index.html')
-    context = {
-        'models': models,
-        'data': data,
-        'picker': data_picker,
-        'graph' : graph,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def Exponential(request):
-    pass
-
-
-
-def makeGraph(model, data):
-    data = data.split(",")
-    if model == 1:
-        x = np.linspace(-5, 5, 100)
-        y = x ** 2
-    if model == 0:
-        x = np.linspace(-5, 5, 100)
-        y = x
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.spines['left'].set_position('center')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # plot the function
-    fig = plt.figure()
-    plt.plot(x, y, 'r')
-
-    imgdata = StringIO()
-    fig.savefig(imgdata, format='svg')
-    imgdata.seek(0)
-
-    data = imgdata.getvalue()
-    return data
-
-"""
