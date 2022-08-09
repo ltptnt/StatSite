7949235d178422a84a11c3738666a60626ba5caf
ltptnt <ltptnt04@gmail.com>
2022-08-09 15:33:35 +1000

    add static files location

--- statsite/settings.py
+++ statsite/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]  # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
