5150e1fb34e8762dfb7ea0f3f9e1efed7df19436
ltptnt <ltptnt04@gmail.com>
2022-08-09 16:35:50 +1000

    added static directories removed whitenoise

--- statsite/settings.py
+++ statsite/settings.py
@@ -119,10 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'StatVisualiser/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/statVisualiser/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
