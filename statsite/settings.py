4ba328347fe8ac9534c5e2855466b2ee4ccdb65d
ltptnt <ltptnt04@gmail.com>
2022-08-10 19:15:25 +1000

    troubleshooting deployment issues

--- statsite/settings.py
+++ statsite/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/statVisualiser/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
